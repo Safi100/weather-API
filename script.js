f39258ff5745e8053ef259482ee1d0f2068d918e
Safi100 <safinafi12@gmail.com>
2023-02-19 22:42:22 +0200

    display data after call api

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ const Form = document.querySelector('form')
 const input = document.querySelector('input')
 const result_container = document.querySelector('.result')
 Form.addEventListener('submit', async (e) => {
+    result_container.textContent = ''
     e.preventDefault()
     const city = input.value.trim()
-    // const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
     .then(response => response.json())
     .then(data => {
         if(data.cod == '404'){
-            console.log("not found")
+            result_container.innerHTML = '<span class="not_found">City Not found</span>'
+            return;
         }
+        const Celcius = parseInt(data.main.temp-273.15);
+        result_container.innerHTML = `
+            <p>Temp: ${Celcius}<sup>°</sup> C</p>
+            <p>${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            Wind:
+            <ul>
+                <li>Speed: ${data.wind.speed}</li>
+                <li>Gust: ${data.wind.gust}</li>
+                <li>Deg: ${data.wind.deg}</li>
+            </ul>
+        `
     })
 })
