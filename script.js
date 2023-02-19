00a5ed9628827a3dfbae59e6310e64f0897581f7
Safi100 <safinafi12@gmail.com>
2023-02-19 21:56:36 +0200

    create api call

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const API_Key = 'c3b390ec825bac1fc7c9779597dde674'
 const Form = document.querySelector('form')
-Form.addEventListener('submit', (e) => {
+const input = document.querySelector('input')
+const result_container = document.querySelector('.result')
+Form.addEventListener('submit', async (e) => {
     e.preventDefault()
-    
+    const city = input.value.trim()
+    // const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data.cod == '404'){
+            console.log("not found")
+        }
+    })
 })
